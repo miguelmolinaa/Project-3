627e4e7899ee75d5e272280426d494172c86d584
cpimentel15 <chelsea.pimentel.valle@gmail.com>
2023-09-27 17:17:00 -0500

    updates

---
+++ static/js/app.js
@@ -0,0 +1,85 @@
+// Sample JSON data (replace this with your actual data)
+const jsonData = [
+    // ... your JSON data here ...
+];
+
+// Function to calculate the price distribution
+function calculatePriceDistribution(data) {
+    // Extract property prices and convert to numeric values
+    const prices = data.map(property => parseFloat(property.Price.replace(',', '')));
+
+    // Create a histogram of property prices
+    const trace = {
+        x: prices,
+        type: 'histogram',
+        marker: { color: 'blue' },
+    };
+
+    const layout = {
+        title: 'Price Distribution',
+        xaxis: { title: 'Price' },
+        yaxis: { title: 'Count' },
+    };
+
+    Plotly.newPlot('price-chart', [trace], layout);
+}
+
+// Function to count properties by state
+function countPropertiesByState(data) {
+    const states = {};
+    
+    data.forEach(property => {
+        const state = property.State;
+        if (states[state]) {
+            states[state]++;
+        } else {
+            states[state] = 1;
+        }
+    });
+
+    const stateNames = Object.keys(states);
+    const propertyCounts = stateNames.map(state => states[state]);
+
+    const trace = {
+        x: stateNames,
+        y: propertyCounts,
+        type: 'bar',
+        marker: { color: 'green' },
+    };
+
+    const layout = {
+        title: 'Properties by State',
+        xaxis: { title: 'State' },
+        yaxis: { title: 'Count' },
+    };
+
+    Plotly.newPlot('state-chart', [trace], layout);
+}
+
+// Function to calculate average price per square foot
+function calculateAveragePricePerSqft(data) {
+    const sqftPrices = data.map(property => {
+        const price = parseFloat(property.Price.replace(',', ''));
+        const sqft = parseFloat(property['Sq.Ft'].replace(',', ''));
+        return sqft > 0 ? price / sqft : 0;
+    });
+
+    const trace = {
+        x: sqftPrices,
+        type: 'histogram',
+        marker: { color: 'orange' },
+    };
+
+    const layout = {
+        title: 'Average Price per Square Foot',
+        xaxis: { title: 'Price per Sq.Ft' },
+        yaxis: { title: 'Count' },
+    };
+
+    Plotly.newPlot('avg-price-chart', [trace], layout);
+}
+
+// Call the functions with your JSON data
+calculatePriceDistribution(jsonData);
+countPropertiesByState(jsonData);
+calculateAveragePricePerSqft(jsonData);
