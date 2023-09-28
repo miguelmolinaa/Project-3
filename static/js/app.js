b250713d3d88fc5fe1024c1de95fbfca56c0afca
cpimentel15 <chelsea.pimentel.valle@gmail.com>
2023-09-27 21:45:55 -0500

    specialist chart added

--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,67 @@
-// Load data from your JSON file (all-listings-json.json)
+// Load data from your JSON file (all-listing-json.json)
+//http://localhost:8000
 d3.json("Resources/all-listing-json.json").then(function(data) {
-    // Extract unique states from the data
-    const states = [...new Set(data.map(item => item.State))];
+    const specialistData = calculateSpecialistData(data);
+
+    // Filter out "Unknown" specialists and empty names
+    const filteredData = specialistData.filter(d => d.specialist !== "Unknown" && d.specialist !== "");
   
-    // Populate the dropdown menu with state options
-    const dropdown = d3.select("#state-dropdown");
-  
-    dropdown
-      .selectAll("option")
-      .data(states)
-      .enter()
-      .append("option")
-      .text(d => d)
-      .attr("value", d => d);
-  
-    // Initialize the charts with all data
-    updateCharts(data);
-  
-    // Add an event listener to the dropdown to update the charts when a state is selected
-    dropdown.on("change", function() {
-      const selectedState = this.value;
-      const filteredData = data.filter(item => item.State === selectedState);
-      updateCharts(filteredData);
-    });
+    // Create a bar chart to display the results
+    createSpecialistBarChart(filteredData);
   });
   
-  function updateCharts(data) {
-    // Update the price-chart (Properties by State - pie chart)
-    const stateCounts = d3.rollup(
+  function calculateSpecialistData(data) {
+    // Group data by specialist and calculate the number of houses sold and the sum of sales for each specialist
+    const specialistData = d3.rollup(
       data,
-      v => v.length,
-      d => d.State
+      v => {
+        return {
+          housesSold: v.length,
+          totalSales: d3.sum(v, d => parseFloat(d.Price.replace(/,/g, "")) || 0),
+        };
+      },
+      d => d.Specialist || "Unknown"
     );
   
-    const stateNames = Array.from(stateCounts.keys());
-    const stateValues = Array.from(stateCounts.values());
-  
-    const priceChartTrace = {
-      labels: stateNames,
-      values: stateValues,
-      type: "pie"
-    };
-  
-    const priceChartData = [priceChartTrace];
-  
-    Plotly.newPlot("price-chart", priceChartData);
-  
-    // Update the avg-price-chart (Average Price per Square Foot)
-    const avgPriceData = data.filter(item => item["Price"] !== "");
-    const avgPricePerSqFt = avgPriceData.map(item => ({
-      State: item.State,
-      AvgPricePerSqFt: parseFloat(item["Price"].replace(/,/g, "")) / parseFloat(item["Sq.Ft"].replace(/,/g, ""))
-    }));
-  
-    const avgPriceTrace = {
-      x: avgPricePerSqFt.map(item => item.State),
-      y: avgPricePerSqFt.map(item => item.AvgPricePerSqFt),
-      type: "bar"
-    };
-  
-    const avgPriceChartData = [avgPriceTrace];
-  
-    Plotly.newPlot("avg-price-chart", avgPriceChartData);
+    // Convert the map to an array of objects
+    return Array.from(specialistData, ([specialist, data]) => ({ specialist, ...data }));
   }
   
+  function createSpecialistBarChart(data) {
+    // Sort the data by the number of houses sold in descending order
+    data.sort((a, b) => b.housesSold - a.housesSold);
+  
+    // Extract the specialist names and sales data
+    const specialists = data.map(d => d.specialist);
+    const housesSold = data.map(d => d.housesSold);
+    const totalSales = data.map(d => d.totalSales);
+  
+    // Create a bar chart
+    const trace1 = {
+      x: specialists,
+      y: housesSold,
+      type: "bar",
+      name: "Houses Sold",
+    };
+  
+    const trace2 = {
+      x: specialists,
+      y: totalSales,
+      type: "bar",
+      name: "Total Sales",
+    };
+  
+    const layout = {
+      title: "Specialist Sales Data",
+      xaxis: {
+        title: "Specialist",
+      },
+      yaxis: {
+        title: "Count/Total Sales",
+      },
+    };
+  
+    const chartData = [trace1, trace2];
+  
+    Plotly.newPlot("specialist-chart", chartData, layout);
+  }
